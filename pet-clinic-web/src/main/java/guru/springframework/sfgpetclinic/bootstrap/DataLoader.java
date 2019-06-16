2a73ba20635616fd97cc71b8464fb084ea86f0fa
Daniel Byun <dbyun@doinglab.com>
2019-06-16 19:25:31 +0900

    Impl Map based Services. Closes #6.

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -0,0 +1,16 @@
+package guru.springframework.sfgpetclinic.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    public DataLoader() {
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
