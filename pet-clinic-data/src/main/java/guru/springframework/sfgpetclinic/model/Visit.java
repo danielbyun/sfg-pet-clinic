5b4527390293859c30495f556dca8c242ef0009e
Daniel Byun <dbyun@doinglab.com>
2019-07-01 09:05:42 +0900

    Added visits, updated owner with pet relationship. Closes #26.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -0,0 +1,35 @@
+package guru.springframework.sfgpetclinic.model;
+
+import javax.persistence.Entity;
+import java.time.LocalDate;
+
+@Entity
+public class Visit extends BaseEntity {
+    private LocalDate localDate;
+    private String description;
+    private Pet pet;
+
+    public LocalDate getLocalDate() {
+        return localDate;
+    }
+
+    public void setLocalDate(LocalDate localDate) {
+        this.localDate = localDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
